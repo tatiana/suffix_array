bd19c861aca34de1a4613e34e65a197b2fe11d1d
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2012-07-29 17:13:04 -0300

    Phase 2 working - disgusting code :/

--- suffix.py
+++ suffix.py
@@ -3,11 +3,12 @@ class SuffixArray(object):
         self._index = 1
 
         self.text = "%s$" % text
-        self.temp_array = []
-        self.group = []
-        self.group_dict = {}
-        self.pending = {}
-        self.group_of_next = []
+        self.temp_array = []  # suffix array wanna be
+        self.group = []  # values based on self.temp_array order
+        self.group_dict = {}  # letter of group: list of indexes
+        self.group_by_index = [None] * len(self.text)  # based on original self.text
+        self.pending = {}  # index in temp_array: number of items to be sorted
+        self.group_of_next = {}  # index in temp_array: next letters of items
 
     def bucket_sort(self, text=None):
         if text is None:
@@ -22,25 +23,81 @@ class SuffixArray(object):
         self.bucket_sort()
         group_indexes = sorted(self.group_dict.keys())
         last_item_index = 0
-        for index in group_indexes:
-            items = self.group_dict[index]
+        for char in group_indexes:
+            items = self.group_dict[char]
             n_items = len(items)
             last_item_index += n_items
 
             index_in_pending = len(self.group)
             if n_items == 1:
-                self.group_dict.pop(index)
+                self.group_dict.pop(char)
             else:
                 self.pending[index_in_pending] = n_items
 
             for i in items:
+                self.group_by_index[i] = last_item_index - 1
                 self.group.append(last_item_index - 1)
 
             self.temp_array += items
 
+    def phase2(self):
+        self.phase1()
+        for index, n_items in self.pending.items():
+            group_of_next = {}
+            array = self.temp_array[index: index + n_items]
+            for value in array:
+                group_of_next[value] = self.group_by_index[value + self._index]
+            sorted_items = sorted(group_of_next, key=group_of_next.get)
+            self.temp_array[index: index + n_items] = sorted_items
+
+
+            if array != sorted_items:
+                # update self.pending
+                self.pending.pop(index)
+                new_pending_group_items = 1
+
+                # update self.group
+                last_index = self.group[index]
+
+                for current_index in xrange(last_index - 1, last_index - n_items, -1):
+                    if group_of_next[self.temp_array[current_index]] != group_of_next[self.temp_array[current_index + 1]]:
+                        self.group[current_index] = current_index
+                        if new_pending_group_items > 1:
+                            self.pending[current_index + 1] = new_pending_group_items
+                        new_pending_group_items = 1
+                    else:
+                        self.group[current_index] = self.group[current_index + 1]
+                        new_pending_group_items += 1
+
+                    if new_pending_group_items > 1:
+                        self.pending[current_index] = new_pending_group_items
+
+
+
+            # index_in_pending = len(self.group)
+            # if n_items != 1:
+            #     self.pending.pop(char)
+            # else:
+            #     self.pending[index_in_pending] = n_items
+
+        self._index *= 2
+
+        # group_indexes = sorted(self.group_dict.keys())
+        # for i in self.pending:
+        #     pass
+        # last_item_index = 0
+        # for index in group_indexes:
+        #     items = self.group_dict[index]
+        #     n_items = len(items)
+        #     last_item_index += n_items
+
+        #     for i in items:
+        #         self.group.append(last_item_index - 1)
+
+        #     self.temp_array += items
 
     def process(self):
-        #self.phase3()
+        self.phase3()
 
         print self.group_of_next
 
--- test_suffix.py
+++ test_suffix.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import unittest
 
 from suffix import SuffixArray
@@ -23,8 +24,9 @@ class TestSuffixArray(unittest.TestCase):
 
     def test_tobeornottobe_phase1(self):
         expected = {
-            'temp': [13, 2, 11, 3, 12, 6, 1, 4, 7, 10, 5, 0, 8, 9],
-            'group': [0, 2, 2, 4, 4, 5, 9, 9, 9, 9, 10, 13, 13, 13],
+            'temp':   [13, 2, 11, 3, 12,  6, 1, 4,  7, 10,  5,  0,  8,  9],
+            'group':  [0,  2,  2, 4,  4,  5, 9, 9,  9,  9, 10, 13, 13, 13],
+            'gindex': [13, 9,  2, 4,  9, 10, 5, 9, 13, 13,  9,  2,  4,  0],
             'pending': {11: 3, 1: 2, 3: 2, 6: 4},
         }
         self.suffix_array.phase1()
@@ -40,19 +42,30 @@ class TestSuffixArray(unittest.TestCase):
             expected['pending'],
             self.suffix_array.pending
         )
+        self.assertEqual(
+            expected['gindex'],
+            self.suffix_array.group_by_index
+        )
 
-    # def test_tobeornottobe_phase2(self):
-    #     expected = {
-    #         #'temp': [2, 11, 3, 12, 1, 4, 7, 10, 0, 8, 9]
-    #         'temp': [13, 2, 11, 12, 3, 6, 1, 10, 4, 7, 5, 0, 8, 9],
-    #         'group': [0, 2,  2,  3, 4, 5, 7, 7,  8, 9, 10, 12, 12, 13],
-    #         'pending': {1: 2, 6: 2, 11: 2}
-    #     }
-    #     self.suffix_array.phase2()
-    #     self.assertEqual(
-    #         expected['temp'],
-    #         self.suffix_array.temp_array
-    #     )
+    def test_tobeornottobe_phase2(self):
+        expected = {
+            'temp': [13, 2, 11, 12, 3, 6, 1, 10, 4, 7, 5, 0, 9, 8],
+            'group': [0, 2,  2,  3, 4, 5, 7, 7,  8, 9, 10, 12, 12, 13],
+            'pending': {1: 2, 6: 2, 11: 2}
+        }
+        self.suffix_array.phase2()
+        self.assertEqual(
+            expected['temp'],
+            self.suffix_array.temp_array
+        )
+        self.assertEqual(
+            expected['group'],
+            self.suffix_array.group
+        )
+        self.assertEqual(
+            expected['pending'],
+            self.suffix_array.pending
+        )
 
     # def test_tobeornottobe(self):
     #     self.assertEqual(
@@ -62,3 +75,16 @@ class TestSuffixArray(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
+# temp[1] == 2
+# temp[2] == 11
+
+# group[1] == 2  # b
+# group[2] == 2  # b
+
+# pending[1] == 2 # dois elementos na posição 1 do temp
+# group_of_next[1] = ['e', 'e']
+
+# {2: 'e', 11: 'e'}
+
